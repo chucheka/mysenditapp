942d219a9b0faaa91e7ef1cad0948d3b209db70f
ucheka22 <ryanucheka@gmail.com>
2020-06-20 18:41:23 +0100

    Updates

---
+++ precache-manifest.bdec79f566d431e8b8360bee61ac2858.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fe7493221a0a73041449c5d880820c2",
+    "url": "/mysenditapp/index.html"
+  },
+  {
+    "revision": "ad90699265cdb4a2b25f",
+    "url": "/mysenditapp/static/css/2.4deadad5.chunk.css"
+  },
+  {
+    "revision": "59cb4f9d6ca7a9c0b911",
+    "url": "/mysenditapp/static/css/main.46ec6f3d.chunk.css"
+  },
+  {
+    "revision": "ad90699265cdb4a2b25f",
+    "url": "/mysenditapp/static/js/2.516cb2ef.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mysenditapp/static/js/2.516cb2ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59cb4f9d6ca7a9c0b911",
+    "url": "/mysenditapp/static/js/main.07f9c44b.chunk.js"
+  },
+  {
+    "revision": "e89b3f2ed11de410b5a9",
+    "url": "/mysenditapp/static/js/runtime-main.906ad0fb.js"
+  },
+  {
+    "revision": "7454bc7276e8aa79d8f2af3de1a595a2",
+    "url": "/mysenditapp/static/media/card.7454bc72.png"
+  },
+  {
+    "revision": "cc41b6268c2d98c52d353a16bc73cf62",
+    "url": "/mysenditapp/static/media/deliveryman.cc41b626.png"
+  },
+  {
+    "revision": "9a9ea6d919cc3a2115814a032132b717",
+    "url": "/mysenditapp/static/media/package.9a9ea6d9.png"
+  },
+  {
+    "revision": "5008d2bcb725df1bbd2461d106c0feaf",
+    "url": "/mysenditapp/static/media/parcel.5008d2bc.png"
+  },
+  {
+    "revision": "4f72509e4c54794fff02b426c1bcf5ec",
+    "url": "/mysenditapp/static/media/price-tag.4f72509e.png"
+  },
+  {
+    "revision": "2d6e8218e317ae19387237c84ada16d8",
+    "url": "/mysenditapp/static/media/save.2d6e8218.png"
+  }
+]);
